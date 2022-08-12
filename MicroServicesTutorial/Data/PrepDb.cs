07321b3ec563fc4e87ade4a71fea9c8bdecfb0d5
guygreenleaf <guygreenleaf@icloud.com>
2022-08-12 10:44:25 -0700

    Add project files.

---
+++ MicroServicesTutorial/Data/PrepDb.cs
@@ -0,0 +1,35 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext? context)
+        {
+            if(context is not null && context.Platforms is not null)
+            {
+                if (!context.Platforms.Any())
+                {
+                    Console.WriteLine("--- Seeding data... ---");
+                    context.Platforms.AddRange(
+                        new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                        new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                        new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                    );
+                    context.SaveChanges();
+                } else
+                {
+                    Console.WriteLine("--- Data already exists In Memory ---");
+                }
+            }
+        }
+    }
+}
---
+++ MicroServicesTutorial/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PlatformService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//Swashbuckler and Swagger DI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "V1" });
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+PrepDb.PrepPopulation(app);
+
+app.Run();
